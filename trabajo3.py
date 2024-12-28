63feb59e30cb9188a4629c2fd0c9c5251ece045d
braian reising <bra.reising@gmail.com>
2024-12-27 22:18:51 -0300

    se realizo el perimetro de un circulo

--- trabajo3.py
+++ trabajo3.py
@@ -1,3 +1,4 @@
+import math
 """ base = int(input("ingrese la base del rectangulo: "))
 altura =int(input("ingrese la altura del rectangulo: ")) """ 
 """ perimetro = 2 * (base + altura) # calcular perimetro en una variable 
@@ -18,10 +19,20 @@ y1= float(input("ingrese el valor de y1: "))
 y2= float(input("ingrese el valor de y2: ")) """
 
 
-def calcular_area(x1,x2,y1,y2):
-    base= abs(x1 - x2)
-    altura= abs(y1 - y2)
+""" def calcular_area(x1,x2,y1,y2):
+    base= abs(x1 - x2)  # abs asegura que sean positvos los valores 
+    altura= abs(y1 - y2)    
     return  base * altura
      
-resultado= calcular_area(1,3,4,5)
-print("el area del rectangulo es: ", resultado)
+resultado = calcular_area(1,3,4,5)
+print("el area del rectangulo es: ", resultado) """
+
+""" radio = int(input("ingrese el perimetro deseado: ")) """
+
+def calcular_perimetro(radio):
+    
+    return 2 * math.pi * radio
+
+
+radio = calcular_perimetro(5)
+print("esto es el perimetro de un rectangulo: ", radio)
