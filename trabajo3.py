f2a4a4e7aa48dec941158dddb2b553eb59cb063f
braian reising <bra.reising@gmail.com>
2024-12-27 22:07:51 -0300

    se hizo el ejercicio de calcular el area de un rectangulo  con ejes x1 y2

--- trabajo3.py
+++ trabajo3.py
@@ -1,16 +1,27 @@
-base = int(input("ingrese la base del rectangulo: "))
-altura =int(input("ingrese la altura del rectangulo: ")) 
-perimetro = 2 * (base + altura) # calcular perimetro en una variable 
-area = base * altura # calcular area en una variable 
-def calcular_area(base,altura): 
+""" base = int(input("ingrese la base del rectangulo: "))
+altura =int(input("ingrese la altura del rectangulo: ")) """ 
+""" perimetro = 2 * (base + altura) # calcular perimetro en una variable 
+area = base * altura # calcular area en una variable  """
+""" def calcular_area(base,altura): 
     return base * altura
 
 def calcular_perimetro(base , altura):
     resultado = 2 * (base + altura)
-    return resultado
+    return resultado """
 
-print("esto es el area de un rectangulo:",calcular_area(2,2))
-print("esto es el perimetro de un rectangulo: ", calcular_perimetro(2,3))
+""" print("esto es el area de un rectangulo:",calcular_area(2,2))
+print("esto es el perimetro de un rectangulo: ", calcular_perimetro(2,3)) """
+
+""" x1= float(input("ingrese el valor de x1: "))
+x2= float(input("ingrese el valor de x2: "))
+y1= float(input("ingrese el valor de y1: "))
+y2= float(input("ingrese el valor de y2: ")) """
 
 
-
+def calcular_area(x1,x2,y1,y2):
+    base= abs(x1 - x2)
+    altura= abs(y1 - y2)
+    return  base * altura
+     
+resultado= calcular_area(1,3,4,5)
+print("el area del rectangulo es: ", resultado)
